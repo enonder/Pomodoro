3670b7d0e64b0ce443fb216c113cdc1bb3a063c8
enonder <elifnonder@gmail.com>
2023-04-01 12:49:21 -0400

    Break function added

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBAction func myButton(_ sender: UIButton) {
         timer.invalidate()
         if(sender.currentTitle == "25:00"){
-            duration = 25 * 60
+            duration = 2 * 5
         } else if(sender.currentTitle == "50:00"){
             duration = 50 * 60
         }
@@ -28,19 +28,41 @@ class ViewController: UIViewController {
  
     }
     
+    func breakTime() {
+        duration = 2 * 3
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(decreaseTimer2), userInfo: nil, repeats: true)
+ 
+    }
+    
     @objc func decreaseTimer() {
         if(duration != 0){
             duration -= 1
             label.text = timeString(time: duration)
-        }else{
+        } else{
             timer.invalidate()
             let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
             player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
+            breakTime()
+            
         }
     }
     
-   
+    @objc func decreaseTimer2() {
+        if(duration != 0){
+            duration -= 1
+            label.text = timeString(time: duration)
+        } else{
+            timer.invalidate()
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            label.text = "Select time!"
+            
+        }
+    }
+    
+    
     func timeString(time: TimeInterval) -> String {
         let minutes = Int(time) / 60 % 60
         let seconds = Int(time) % 60
